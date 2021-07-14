be76bc23d11288b4505f55cae2a0158be26391fb
EternallyCoding <87423860+EternallyCoding@users.noreply.github.com>
2021-07-14 07:23:54 -0500

    Add files via upload
    Finds computers on local domain that have not been logged in to for the past year and moves them to an Obsoleted Computers OU

---
+++ Obsoleted Computers Script.ps1
@@ -0,0 +1,9 @@
+#Script finds every computer in AD that has not been logged in to for the past year and moves them to the Obsoleted Computers OU
+#Increase the absolute value of -365 to go back farther
+#Change Get-ADComputer to Get-ADUser and OU=Obsoleted Computers to OU=Obsoleted Users for Users
+
+import-module activedirectory
+
+$datecutoff = (Get-Date).AddDays(-365)
+
+Get-ADComputer -Properties LastLogonDate -Filter {LastLogonDate -lt $datecutoff} | Move-ADObject -TargetPath "OU=Obsoleted Computers,DC=mddomain,DC=local"
