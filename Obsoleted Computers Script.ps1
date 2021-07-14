4ddc7c06a19253b5d9590949dc62cc8ca70fb616
EternallyCoding <87423860+EternallyCoding@users.noreply.github.com>
2021-07-14 07:32:03 -0500

    Find Old Domain Computers

--- Obsoleted Computers Script.ps1
+++ Obsoleted Computers Script.ps1
@@ -1,5 +1,7 @@
 #Script finds every computer in AD that has not been logged in to for the past year and moves them to the Obsoleted Computers OU
+
 #Increase the absolute value of -365 to go back farther
+
 #Change Get-ADComputer to Get-ADUser and OU=Obsoleted Computers to OU=Obsoleted Users for Users
 
 import-module activedirectory
