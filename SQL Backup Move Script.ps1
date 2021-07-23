192d63c1e5a6a0a13938047e9a1cf4527c991d70
EternallyCoding <87423860+EternallyCoding@users.noreply.github.com>
2021-07-23 13:58:13 -0500

    Update SQL Backup Move Script.ps1

--- SQL Backup Move Script.ps1
+++ SQL Backup Move Script.ps1
@@ -4,6 +4,9 @@ $Start = "\\Server\Share\Folder\Subfolder"
 #Location of where the files are to be moved
 $End = "\\Server\Share\Folder\Subfolder"
 
+#Deletes all files in destination folder, does not remove folders
+Remove-Item -Path $End\*.*
+
 #Change the Filter and Property as needed to find the correct file type and sorting option
 Get-ChildItem $Start | ForEach-Object {$Folder = $_} `
 {$Item = Get-ChildItem $Start\$Folder -Filter *.bak | Sort-Object -Property LastWriteTime | Select-Object -Last 1} `
