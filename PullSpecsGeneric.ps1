f2f0c64d5c0f11d17f70bd0eb2344f5a8fb7b376
EternallyCoding <87423860+EternallyCoding@users.noreply.github.com>
2024-07-03 14:48:57 -0500

    Update PullSpecsGeneric.ps1

--- PullSpecsGeneric.ps1
+++ PullSpecsGeneric.ps1
@@ -28,6 +28,7 @@ $Addresses = Get-NetAdapter | select MacAddress | Out-String
 #Replace 'XXX.YYY' in the -From and -To fields with the email addresses you wish to send From and To, and change instances of $User in both fields as needed.
 #Replace ZZZ with your domain 
 Send-MailMessage -From "$User <$User@XXX.YYY>" -To '$User <$User@XXX.YYY>' -Subject 'Computer Specs' -Body ("Computer name is " + $Hostname + "`n`n" + "Model is " + $ModelType + "`n`n" + "Serial Number is " + $SerialNumber + "`n`n" + "CPU is " + $Processor + "`n`n" + "RAM is " + $Memory + "`n`n" + "Hard Drive(s) are " + $TotalDisks + "`n`n" + "GPU is " + $TotalGPUs + "`n`n" + $Addresses) -Credential ZZZ\$User
+New-Item -Path ~\Desktop -Name "specs.txt" -ItemType "file" -Value ("Serial Number is " + $SerialNumber + "`n`n" + "Model is " + $ModelType + "`n`n" + "Hostname is " + $Hostname + "`n" + "CPU is " + $Processor + "`n`n" + "RAM is " + $Memory + "`n`n" + "Hard Drive(s) are " + $TotalDisks + "`n" + "GPU is " + $TotalGPUs + "`n" + $Addresses)
 
 echo("Email has (likely) been sent. Press enter to exit")
 
